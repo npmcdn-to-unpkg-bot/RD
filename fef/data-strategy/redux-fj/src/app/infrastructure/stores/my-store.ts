7813df5305da47902a0bd408aafb88ad10810467
Franz Junge <bitbucket@junge.f9.co.uk>
2016-04-05 11:32:47 +0100

    Removed redundant code

--- fef/data-strategy/redux-fj/src/app/infrastructure/stores/my-store.ts
+++ fef/data-strategy/redux-fj/src/app/infrastructure/stores/my-store.ts
@@ -1,15 +1,10 @@
 import {Injectable} from "angular2/core";
-import {List} from "immutable";
 import {createStore} from "redux";
 import {combineReducers} from 'redux';
 
 import {ReduxStore} from "./redux-store";
 import {Mocks} from "./mocks";
 
-import {Product} from "../models/product";
-import {Purchase} from "../models/purchase";
-import {UiState} from "../models/ui-state";
-import {IActionBase} from "../actions/actions-bas";
 import {products} from "../reducers/product-reducer";
 import {uiState} from "../reducers/ui-state-reducer";
 import {basket} from "../reducers/purchase-reducer";
@@ -34,27 +29,5 @@ export class MyStore extends ReduxStore {
 	constructor() {
 		super(store); 
 	}
-	
-	// get catalog(): Array<Product> {
-	// 	let l: List<Product> = <List<Product>> this._store.getState().products; 
-
-	// 	return l.toArray();
-	// }
-	
-	// get basket(): Array<Purchase> {
-	// 	let l: List<Purchase> = <List<Purchase>> this._store.getState().basket;
-		
-	// 	return l.toArray();
-	// }
-	
-	// get uiState(): UiState {
-	// 	return this._store.getState().uiState;
-	// }
-
-	// dispatch(action: IActionBase): void {
-	// 	this._store.dispatch(action);
-	// }
-	
-
 	
 }
